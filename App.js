f7658d3530fa07fea7e6f82dfd855440f5f0e08e
unknown <ramonarents@hotmail.com>
2024-03-19 22:51:46 +0100

    add button and search function

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
   FlatList,
   ImageBackground,
+  TouchableOpacity,
 } from "react-native";
 import Card from "./components/Card";
 import SearchComponent from "./components/Search";
+import { Icon } from "@rneui/themed";
 
 export default function App() {
+  
+  const [searchQuery, setSearchQuery] = useState("");
 
   //TODO: Voorbeeld data voor de style. Uiteindelijk moet dit met een API gaan werken.
-  const data = [
+  const [data, setData] = useState([
     { id: "1", title: "Wisen Rosen", book: "Keyboard speel je zo", blz: "10" },
     { id: "2", title: "Apache", book: "New West", blz: "20" },
     { id: "3", title: "Sweet Caroline", book: "Neil Diamond", blz: "130" },
@@ -19,11 +24,28 @@ export default function App() {
     { id: "5", title: "Apache", book: "New West", blz: "20" },
     { id: "6", title: "Sweet Caroline", book: "Neil Diamond", blz: "130" },
     { id: "7", title: "Apache", book: "New West", blz: "20" },
-  ];
+  ]);
+
+  const [filteredData, setFilteredData] = useState(data);
 
   const handleSearch = (searchText) => {
     console.log("Search Text:", searchText);
     // TODO: Moet de cards gaan zoeken
+    if(searchText === ""){
+      setFilteredData(data);
+    } else {
+      const newData = data.filter(item => {
+        const itemName = item.title.toLowerCase();
+        const text = searchText.toLowerCase();
+        return itemName.indexOf(text) > -1;
+      });
+      setFilteredData(newData);
+    }
+  };
+
+  const handleOnPress = () => {
+    //TODO: Link maken naar add page
+    console.log("Pressed");
   };
 
   return (
@@ -36,9 +58,10 @@ export default function App() {
         style={styles.background}
       >
         <View style={styles.overlay}>
-          <SearchComponent onSearch={handleSearch} />
+        {/* TODO: Uitzoeken hoe we gelijk kunnen zoeken als we de tekst intikken. */}
+          <SearchComponent onSearch={handleSearch} value={searchQuery} />
             <FlatList
-              data={data}
+              data={filteredData}
               renderItem={({ item }) => (
                 <Card
                   title={item.title}
@@ -49,6 +72,11 @@ export default function App() {
               keyExtractor={(item) => item.id}
               contentContainerStyle={styles.flatlist}
             />
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.button} onPress={handleOnPress}>
+                <Icon name="add" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
             {/* Trucje om het laatste item in de list zichtbaar te houden */}
             <Text></Text>
             <Text></Text>
@@ -65,6 +93,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  buttonContainer:{
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end', 
+    marginBottom: 25, 
+    marginRight: 16,
+  },
   background: {
     height: "100%",
     resizeMode: "cover",
@@ -79,7 +114,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#1d3275",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: 20, // Adjust this value if you need more space for status bar
+    paddingTop: 20, 
   },
   headerText: {
     color: "white",
@@ -91,5 +126,18 @@ const styles = StyleSheet.create({
   },
   hidden:{
     opacity: 0
-  }
+  },
+  button: {
+    backgroundColor: '#1d3275', 
+    width: 65,
+    height: 65,
+    borderRadius: 37, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 4, 
+    shadowColor: '#000', 
+    shadowOffset: { width: 0, height: 2 }, 
+    shadowOpacity: 0.4, 
+    shadowRadius: 3, 
+  },
 });
