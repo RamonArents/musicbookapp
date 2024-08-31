afe4ef9d3872f8155dec06039af0b1151efd8621
unknown <ramonarents@hotmail.com>
2024-08-31 16:23:51 +0200

    Added sql lite database

--- Views/Add.jsx
+++ Views/Add.jsx
@@ -6,8 +6,50 @@ import {
   ImageBackground,
 } from "react-native";
 import styles from "../styles/Style";
+import {
+  openDatabase,
+  insertBook,
+  createTable,
+} from "../controllers/db";
+import { useEffect, useState } from "react";
+
 
 export default function Add() {
+  const [title, setTitle] = useState("");
+  const [book, setBook] = useState("");
+  const [blz, setBlz] = useState("");
+  const [db, setDb] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const setupDatabase = async () => {
+      try{
+      //TODO: The database doesn't seem to open (getting null object). We have to fix this.
+      const dbInstance = await openDatabase();
+      setDb(dbInstance);
+      createTable(dbInstance);
+      }catch(error){
+        setError(error.message);
+        console.error("Failed to open database: " + error);
+      }
+    };
+
+    setupDatabase();
+  }, []);
+
+  const handleOnPress = async () => {
+    try {
+      await insertBook(db, title, book, blz);
+      setTitle("");
+      setBook("");
+      setBlz("");
+
+      console.log("Book saved succesfully");
+    } catch (error) {
+      console.error("Error saving book: ", error);
+    }
+  };
+
   return (
     <View style={styles.flex1}>
       <ImageBackground
@@ -17,12 +59,27 @@ export default function Add() {
         <View style={[styles.overlay, styles.overlayPosition]}>
           <View style={styles.form}>
             <Text style={[styles.labels, styles.colorWhite]}>Titel:</Text>
-            <TextInput style={styles.input} placeholder="Titel" />
+            <TextInput
+              style={styles.input}
+              placeholder="Titel"
+              value={title}
+              onChangeText={setTitle}
+            />
             <Text style={[styles.labels, styles.colorWhite]}>Boek:</Text>
-            <TextInput style={styles.input} placeholder="Boek" />
+            <TextInput
+              style={styles.input}
+              placeholder="Boek"
+              value={book}
+              onChangeText={setBook}
+            />
             <Text style={[styles.labels, styles.colorWhite]}>Bladzijde:</Text>
-            <TextInput style={styles.input} placeholder="Blz" />
-            <TouchableOpacity style={styles.button}>
+            <TextInput
+              style={styles.input}
+              placeholder="Blz"
+              value={blz}
+              onChangeText={setBlz}
+            />
+            <TouchableOpacity style={styles.button} onPress={handleOnPress}>
               <Text style={styles.colorWhite}>Toevoegen</Text>
             </TouchableOpacity>
           </View>
